3408f983bd6dee9cdaac7fe152a49ea688505b5c
Arijana Brlek <arijanabrlek@gmail.com>
2016-04-10 01:32:52 +0200

    Edit modal

--- resources/views/schedule/home.blade.php
+++ resources/views/schedule/home.blade.php
@@ -55,6 +55,7 @@
 
 @endsection
 
+
 @section('scripts')
     @parent
     <script type="text/javascript" src="{{ URL::asset('plugins/datatables/jquery.dataTables.min.js') }}"></script>
@@ -84,7 +85,6 @@
 
         });
 
-
     </script>
 
     <script>
@@ -103,7 +103,6 @@
             });
         });
     </script>
-    <link href="{{ asset('plugins/select2/select2.min.css') }}" rel="stylesheet">
     <script type="text/javascript" src="{{ URL::asset('plugins/select2/select2.min.js') }}"></script>
 
 @endsection
--- resources/views/schedule/partials/modal.blade.php
+++ resources/views/schedule/partials/modal.blade.php
@@ -5,8 +5,9 @@
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
                 <h4 class="modal-title" id="myModalLabel">Details</h4>
             </div>
+
             <div class="modal-body">
-                <div>
+
                     {{ $schedule->id }}
                     @foreach($stations as $station)
                         {{ $station->id }}
@@ -15,13 +16,13 @@
                         {{ $task_type }}
                     @endforeach
 
-{{--                    {!! Form::model($station, ['method' => 'PATCH', 'url' => ['schedule', $station->id], 'class' => 'form-horizontal']) !!}--}}
-                    {!! Form::model($station, ['method' => 'PATCH', 'class' => 'form-horizontal']) !!}
+                    {{--{!! Form::model($station, ['method' => 'PATCH', 'url' => ['schedule', $station->id], 'class' => 'form-horizontal']) !!}--}}
+                    {!! Form::model($station, ['method' => 'PATCH', 'class' => '']) !!}
 
-                    <div class="form-group">
-                        {!! Form::label('team1', 'Station', ['class' => 'col-sm-3 control-label']) !!}
-                        <div class="col-sm-6">
-                            <select name="primary" class="form-control select2 station" multiple="multiple" data-placeholder="Select employee type">
+                    <div class="form-group full-width">
+                        {!! Form::label('stations', 'Station', ['class' => 'col-sm-3 control-label']) !!}
+                        <div class="col-sm-6 full-width">
+                            <select name="primary" class="form-control select2 station" data-placeholder="Station">
                                 @foreach($stations as $station)
                                     <option value="{{ $station->id }}">{{ $station->name }}</option>
                                 @endforeach
@@ -29,10 +30,10 @@
                         </div>
                     </div>
 
-                    <div class="form-group">
+                    <div class="form-group full-width">
                         {!! Form::label('STATUS', 'Status', ['class' => 'col-sm-3 control-label']) !!}
                         <div class="col-sm-6">
-                            <select name="primary" class="form-control select2" multiple="multiple" data-placeholder="Select employee type">
+                            <select name="primary" class="form-control select2" data-placeholder="Employee type">
                                 <option value="D">Day</option>
                                 <option value="N">Night</option>
                                 <option value="_">Day off</option>
@@ -40,16 +41,15 @@
                         </div>
                     </div>
 
-                    <div class="form-group">
-                        {!! Form::label('team', 'Status', ['class' => 'col-sm-3 control-label']) !!}
+                    <div class="form-group full-width">
+                        {!! Form::label('team', 'Shift', ['class' => 'col-sm-3 control-label']) !!}
                         <div class="col-sm-6">
-                            <select name="primary" class="form-control team-select select2" multiple="multiple" data-placeholder="Select employee type">
+                            <select name="primary" class="form-control select2 team-select" multiple="multiple" data-placeholder="Select employee shift">
 
                             </select>
                         </div>
                     </div>
 
-                </div>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
@@ -66,16 +66,20 @@
 <script>
     $(".select2").select2();
     $(function(){
+
         $('.station').change(function(e) {
-            var cuisines = ["Chinese", "Indian"];
+
+            console.log("dskjfjdks");
+
+            var shifts = ["Day", "Indian"];
             var sel = $('form .team-select')[0];
 
             $('form .team-select').select2("val", "");
             sel.options.length = 0;
-            for(var i = 0; i < cuisines.length; i++) {
+            for(var i = 0; i < shifts.length; i++) {
                 var opt = document.createElement('option');
-                opt.innerHTML = cuisines[i];
-                opt.value = cuisines[i];
+                opt.innerHTML = shifts[i];
+                opt.value = shifts[i];
                 sel.appendChild(opt);
             }
         });
