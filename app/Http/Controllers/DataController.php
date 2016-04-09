4e76a2ccafdd61e08f0031070c8012ff1743d602
Arijana Brlek <arijanabrlek@gmail.com>
2016-04-09 14:54:38 +0200

    Begin input file generator

---
+++ app/Http/Controllers/DataController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Station;
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class DataController extends Controller
+{
+    public function createIputFile()
+    {
+        $filename = "";
+
+        //num of weeks
+        $n = 4;
+
+        //num of stations
+        $stations = Station::all();
+        $m = $stations.count();
+
+        foreach ($stations as $station) {
+
+
+        }
+
+
+    }
+}
