703a3192b536833719fbb0db54fe9b27c20e23cb
Arijana Brlek <arijanabrlek@gmail.com>
2016-04-10 04:56:50 +0200

    Add chart to employee profile

--- resources/views/employees/report.blade.php
+++ resources/views/employees/report.blade.php
@@ -126,7 +126,8 @@
                             </div>
                         </div>
                         <div class="row">
-                            <canvas></canvas>
+                            <canvas id="chart" style="height: 250px;"></canvas>
+
                         </div>
 </div>
                 </div>
@@ -144,6 +145,8 @@
     <script type="text/javascript" src="{{ URL::asset('plugins/datatables/jquery.dataTables.min.js') }}"></script>
     <script type="text/javascript" src="{{ URL::asset('plugins/datatables/dataTables.bootstrap.min.js') }}"></script>
     <script type="text/javascript" src="{{ URL::asset('plugins/select2/select2.min.js') }}"></script>
+    <script type="text/javascript" src="{{ URL::asset('plugins/chartjs/Chart.min.js') }}"></script>
+
     <script>
         $(document).ready(function () {
 
@@ -171,7 +174,82 @@
                 "sort": true
             });
 
+
+
+
+
         });
+
+
+        //-------------
+        //- PIE CHART -
+        //-------------
+        // Get context with jQuery - using jQuery's .get() method.
+        var pieChartCanvas = $("#chart").get(0).getContext("2d");
+        var pieChart = new Chart(pieChartCanvas);
+        var PieData = [
+            {
+                value: 700,
+                color: "#f56954",
+                highlight: "#f56954",
+                label: "Chrome"
+            },
+            {
+                value: 500,
+                color: "#00a65a",
+                highlight: "#00a65a",
+                label: "IE"
+            },
+            {
+                value: 400,
+                color: "#f39c12",
+                highlight: "#f39c12",
+                label: "FireFox"
+            },
+            {
+                value: 600,
+                color: "#00c0ef",
+                highlight: "#00c0ef",
+                label: "Safari"
+            },
+            {
+                value: 300,
+                color: "#3c8dbc",
+                highlight: "#3c8dbc",
+                label: "Opera"
+            },
+            {
+                value: 100,
+                color: "#d2d6de",
+                highlight: "#d2d6de",
+                label: "Navigator"
+            }
+        ];
+        var pieOptions = {
+            //Boolean - Whether we should show a stroke on each segment
+            segmentShowStroke: true,
+            //String - The colour of each segment stroke
+            segmentStrokeColor: "#fff",
+            //Number - The width of each segment stroke
+            segmentStrokeWidth: 2,
+            //Number - The percentage of the chart that we cut out of the middle
+            percentageInnerCutout: 50, // This is 0 for Pie charts
+            //Number - Amount of animation steps
+            animationSteps: 100,
+            //String - Animation easing effect
+            animationEasing: "easeOutBounce",
+            //Boolean - Whether we animate the rotation of the Doughnut
+            animateRotate: true,
+            //Boolean - Whether we animate scaling the Doughnut from the centre
+            animateScale: false,
+            //Boolean - whether to make the chart responsive to window resizing
+            responsive: true,
+            // Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
+            maintainAspectRatio: true,
+    };
+
+
+        pieChart.Doughnut(PieData, pieOptions);
 
     </script>
 
