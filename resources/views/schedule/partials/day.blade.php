52981a65205d35524be1a8a16fa2263a446ec39f
Arijana Brlek <arijanabrlek@gmail.com>
2016-04-10 09:01:34 +0200

    Remove landing page

--- resources/views/schedule/partials/day.blade.php
+++ resources/views/schedule/partials/day.blade.php
@@ -1,10 +1,10 @@
 <div class="shift day-shift no-padding no-margin" data-id="{{ $schedule->id }}" data-type="" data-station-id="">
-    <button type="button" class="btn btn-warning btn-flat" data-target="#myModal">
+    <button type="button" class="btn btn-warning btn-flat no-padding no-margin" data-target="#myModal">
         <i class="fa fa-sun-o icon-small"></i>
         <br/>
-        <small>{{ $schedule->station->name }}</small>
-        <br/>
         <small>{{ $schedule->employee_task->name }}</small>
+        <br/>
+        <small class="no-margin no-padding text-wrap">{{ $schedule->station->name }}</small>
     </button>
 </div>
 <div id="myModal"></div>
--- resources/views/schedule/partials/night.blade.php
+++ resources/views/schedule/partials/night.blade.php
@@ -1,6 +1,10 @@
 <div class="shift night-shift" data-id="{{ $schedule->id }}" data-type="" data-station-id="">
     <button type="button" class="btn btn-primary btn-flat" data-target="#myModal">
         <i class="fa fa-moon-o"></i>
+        <br/>
+        <small>{{ $schedule->employee_task->name }}</small>
+        <br/>
+        <small class="no-margin no-padding text-wrap">{{ $schedule->station->name }}</small>
     </button>
 </div>
 
